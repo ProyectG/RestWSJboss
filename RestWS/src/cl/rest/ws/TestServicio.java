6ecbf3cbbd61d7cb5a97c2c617476dc1645ca3ec
Marco Saffie <marco.saffie@enhance.cl>
2017-11-17 16:55:41 -0300

    version de prueba que acepta xml. no funciona todavia eso creo.

--- RestWS/src/cl/rest/ws/TestServicio.java
+++ RestWS/src/cl/rest/ws/TestServicio.java
@@ -4,6 +4,7 @@ package cl.rest.ws;
 import java.util.List;
 
 import javax.persistence.Query;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -16,24 +17,34 @@ import javax.ws.rs.core.Response;
 public class TestServicio {
 	
 	
+	 	@Path("both")
+	    @POST
+	    @Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	 	public void printMessage(Student s)
+	    {
+	        System.out.println("****Student Details*****");
+	        System.out.println("Student Name : "+s.getName());
+	        System.out.println("Student Age  : "+s.getAge());
+	    }
+	
 
-	@GET
-	@Path("/{param}/{param1}")
-	@Produces({"application/xml"})
-	public Response printMessage(@PathParam("param") String nombre, @PathParam("param1") String apellido) {
-
-		
-		String result = "Hola "+nombre+" "+apellido;
-		//return result;
-		
-		//Retorno texto
-		return Response.status(200).entity(result).build();
-		
-		//Retorno XML
-		 //return Response.status(200).type(MediaType.APPLICATION_XML).entity(resu).build();
-		
-
-	}
+//	@GET
+//	@Path("/{param}/{param1}")
+//	@Produces({"application/xml"})
+//	public Response printMessage(@PathParam("param") String nombre, @PathParam("param1") String apellido) {
+//
+//		
+//		String result = "Hola "+nombre+" "+apellido;
+//		//return result;
+//		
+//		//Retorno texto
+//		return Response.status(200).entity(result).build();
+//		
+//		//Retorno XML
+//		 //return Response.status(200).type(MediaType.APPLICATION_XML).entity(resu).build();
+//		
+//
+//	}
 
 
 	
